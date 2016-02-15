88469f40e7bd1b569893a2dad5d099d97a2befc5
shmulim <shmuel.markel@gmail.com>
2016-02-14 21:27:05 -0800

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-  // call functions here
+  submitForm();
 });
 
-// define functions here
+function submitForm(){
+  jQuery('form').on('submit', function(event){
+    var task = jQuery('input#item').val();
+    jQuery('ol').append('<li>' + task + '</li>');
+    event.preventDefault();
+  });
+}
