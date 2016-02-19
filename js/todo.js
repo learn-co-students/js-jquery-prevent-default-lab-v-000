716316ed9cb0e121af68b43a82961df92b4d0d79
John Anthony Rivera <askinganthony@gmail.com>
2016-02-18 16:41:08 -0800

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+function submitForm(){
+	$('form').submit(function(event){
+		event.preventDefault();
+		$('#list ol').append( '<li>'+ $('#item').val() +'</li>' );
+	});
+}
