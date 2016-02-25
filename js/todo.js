0a7baed4c8ff9a04848c3ea0fad9513855699b55
aenser <aaronenser1@gmail.com>
2016-02-25 14:24:53 -0700

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+// function submitForm() {
+//   $(':submit').click(function(event) {
+//     var addTo = $('#item').val();
+//     $('#list').append('<li>' + addTo + '</li>');
+//     event.preventDefault(); 
+//   });
+// }
+
+// This ^^^^ works, but not passing the Learn tests
+
+function submitForm() {
+  $('form').on('submit', function(event){
+    var item = $('#item').val();
+  $('#list ol').append('<li>' + item + "</li>");
+  event.preventDefault();
+  })
+}
