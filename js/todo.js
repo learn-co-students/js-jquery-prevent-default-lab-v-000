c3eb2676be4c3cbf865d347c3511a8c1dfa4020c
pajamaw <pjpeterww@gmail.com>
2016-03-21 14:09:14 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
   // call functions here
+  addItem();
 });
+function submitForm(){
+ $('form').on('submit', function(event){
+    var item = $('#item').val();
+   $("#list ol").append('<li>'+item+'</li><br />');
+    event.preventDefault();
+   });
 
-// define functions here
+}
