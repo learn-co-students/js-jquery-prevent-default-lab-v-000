f8a8b6c9dd0194873e3997e47052705a9bc58dea
Rawradin <edgar.rawr@gmail.com>
2016-03-30 08:28:01 -0700

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+var submitForm = function(){
+  $("form").on('submit', function(event){
+    var todoItem = $("input").val();
+    $("ol").append("<li>" + todoItem + "</li>");
+    event.preventDefault();
+  })
+}
