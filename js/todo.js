3b7653de7e1cbd010dca0c10d39326ee2cff0bb2
mmmeyers <marsi286@gmail.com>
2016-04-06 18:37:30 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
+
+  submitForm();
+
   // call functions here
 });
 
 // define functions here
+function submitForm() {
+  $('form').on('submit', function(event) {
+    var item = $('#item').val();
+    $('#list ol').append('<li>' + item + '</li>');
+    event.preventDefault();
+  });
+}
