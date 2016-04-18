e7c056962ca007845bcde32bce855ef554b81a44
viparthasarathy <viparthasarathy@gmail.com>
2016-04-18 12:31:15 -0700

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-  // call functions here
+  submitForm();
 });
 
-// define functions here
+function submitForm() {
+  $('form').on('submit', addListItem);
+}
+
+function addListItem(e) {
+  var item = $('#item').val();
+  $('#list ol').append('<li>' + item + '</li>');
+}
