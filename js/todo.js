19ea31f8c5daf09b97b13575a963f15d772595a2
jjlippman <jjlippman@earthlink.net>
2016-04-18 13:11:53 -0600

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function(e){
   // call functions here
+      listItems();
 });
 
 // define functions here
+
+function listItems(){
+$('form').on('submit', function(event){
+    value = $('#item').val();
+    $('ol').text(value);
+   
+}
+
+})
+   
