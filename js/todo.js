3c639383a57605ae5cd2e55094d4e0c71c33121f
azheng249 <andy.zheng249@gmail.com>
2016-04-24 14:11:05 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-  // call functions here
+  submitForm();
 });
 
 // define functions here
+function submitForm () {
+  $("form").on("submit", function(e){
+    e.preventDefault();
+
+    var toDo = $("#item").val();
+    $("#list ol").append("<li>" + toDo + "</li>");
+
+    $("#item").val("");
+  });
+}
