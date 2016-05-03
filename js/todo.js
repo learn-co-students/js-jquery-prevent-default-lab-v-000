04ace9945ac4b053c3efc95625338956aa19112b
rocketNeck <homestarflyer@gmail.com>
2016-05-03 19:41:25 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-  // call functions here
+  submitForm();
 });
 
-// define functions here
+function submitForm() {
+$('form').on("submit", function(event){
+  var formitem = $('#item').val();
+  $('ol').append('<li>' + formitem + '</li>');
+  $('#item').val("")
+  event.preventDefault();
+  });
+}
