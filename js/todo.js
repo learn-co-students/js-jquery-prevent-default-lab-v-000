3e65a4c9257225e5fca4b50557fb4bd8787d9907
Enoch Griffith <enoch2k2@hotmail.com>
2016-05-17 20:39:49 -0500

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-  // call functions here
+  submitForm();
 });
 
-// define functions here
+function submitForm() {
+  $('form').on('submit', function(){
+    var item = $('#item').val();
+    $("div#list ol").append("<li>" + item + "</li>");
+    event.preventDefault();
+  });
+}
