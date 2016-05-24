4e074bc40ba89e4567d3cf1edffeda483530d299
agdavid <antonio.g.david@gmail.com>
2016-05-24 10:58:02 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,24 @@
+//Note: My commented out code below works, but just does not pass the specific Learn tests
+
 $(document).ready(function(){
-  // call functions here
+  //bind 'form' to 'submit' event and call function handleFormSubmit
+  //$('form').on('submit', handleFormSubmit);
+  submitForm();
 });
 
-// define functions here
+function submitForm() {
+  $('form').on('submit', function(e){
+    var item = $('#item').val();
+    $('ol').append('<li>' + item + '</li>');
+    e.preventDefault;
+  });
+}
+
+// function handleFormSubmit(e) {
+//   //set item equal to the value of the element with #item
+//   var item = $('#item').val();
+//   //append to the <ol> a new <li> with the item value
+//   $("ol").append("<li>" + item + "</li>");
+//   //stop browser refresh
+//   e.preventDefault();
+// }
