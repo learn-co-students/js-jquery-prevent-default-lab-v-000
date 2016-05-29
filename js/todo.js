1f76177e6145f2314d5f6b47f34479cb9d58a73a
seanliu93 <sean93liu@gmail.com>
2016-05-29 13:56:51 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+  submitForm();
   // call functions here
 });
 
 // define functions here
+function submitForm() {
+$('form').on('submit', function(event){
+ var data = $('#item').val();
+ $('ol').append( '<li>' + data + '</li>' );
+ event.preventDefault();
+})}
