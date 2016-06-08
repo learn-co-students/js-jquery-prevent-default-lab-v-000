6bd6061c9c561ba180d5acdb72613ecf5517cab7
Llubomir Stanojlovic <llustanojlovic@gmail.com>
2016-06-08 22:23:06 +0000

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
   // call functions here
+  asd()
 });
 
 // define functions here
-function (){
+//class .
+//id    #
+function asd(){
   $(':submit').on('click', function(event){
-    $('#list ol').append("<li>" +$('#item').val()+"</li>");
+    $('#list ol').append("<li>" + $('#item').val() + "</li>")
     event.preventDefault();
   });
 }
