7fff96ab3ffee25af39d7165a4ba219459d6fcd2
Llubomir Stanojlovic <llustanojlovic@gmail.com>
2016-06-09 01:05:13 +0000

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,14 +1,24 @@
 $(document).ready(function(){
   // call functions here
-  asd()
+  submitForm()
 });
 
 // define functions here
 //class .
 //id    #
-function asd(){
-  $(':submit').on('click', function(event){
-    $('#list ol').append("<li>" + $('#item').val() + "</li>")
+function submitForm(){
+  // :submit to select button and do stuff it being clicked
+  $('form').on('submit', function(event){
+    var item = $('#item').val();
+    //using the ordered list append the information from the textbox to the list
+    //$('#list ol').append("<li>" + $('#item').val() + "</li>");
+    add(item);
+    //all this without refreshing the webpage.
     event.preventDefault();
+    //default functionality of submit button is the refresh the webpage.
   });
+}
+
+function add(item){
+  $('#list ol').append("<li>" + item + "</li>");
 }
