24aaa18176f4d6cf7c3687f58fa3a4ae440ba604
Peter Jacobson <github email address>
2016-07-12 11:50:22 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   // call functions here
+  submit();
 });
 
 // define functions here
+
+function submit(){
+$( 'form' ).submit(function( event ) {
+   var item = $("#item").val();
+   $('ol').append('<li>' + item + '</li>');
+  event.preventDefault();
+});
+}
