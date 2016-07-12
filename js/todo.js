a7b189b1405585acb90925cfb4af1a3d819fc74b
Peter Jacobson <github email address>
2016-07-12 11:52:08 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-  // call functions here
-  submit();
+  
+  submitForm();
 });
 
-// define functions here
 
-function submit(){
-$( 'form' ).submit(function( event ) {
+
+function submitForm(){
+$( 'form' ).on('submit', function( event ) {
    var item = $("#item").val();
    $('ol').append('<li>' + item + '</li>');
   event.preventDefault();
