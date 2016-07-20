739d166a2bfd9403c386f7382f0ec1e54969e24c
hudbuz <hzb2@cornell.edu>
2016-07-20 16:34:52 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
+  submitForm()
   // call functions here
 });
 
 // define functions here
+function submitForm() {
+  $('form').on('submit', adds)
+  
+
+}
+function adds(e) {
+
+  
+    var name = $('#item').val()
+    $('#list ol').append("<li>"+ name +"</li>")
+    event.preventDefault()
+    
+    
+
+}
