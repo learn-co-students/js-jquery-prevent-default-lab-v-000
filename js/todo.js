7407a81088a3dcf5f968e62691061930dd9149b3
Heber Sandoval <he3ers@gmail.com>
2016-07-21 10:33:28 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+function submitForm() {
+  // $('form').on('submit', adds);
+  $('form').on('submit', function (event) {
+    var item = $('#item').val();
+    $('#list ol').append("<li>" + item + "</li>");
+    event.preventDefault();
+  });
+}
+
+// function adds(event) {
+//   var item = $('#item').val();
+//   $('#list ol').append("<li>" + item + "</li>");
+//   event.preventDefault();
+// }
