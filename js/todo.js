a59c81de9ab674c980715425cb1136ed19b8b333
theRingleman <sam.ringleman@gmail.com>
2016-07-27 11:44:09 -0600

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-  // call functions here
+  submitForm();
 });
 
 // define functions here
+function submitForm() {
+  $('form').on("submit", handleSubmitEvent)
+}
+
+function handleSubmitEvent(event) {
+  var item = $("#item").val();
+  $('ol').append('<li>' + item + "</li>");
+  event.preventDefault();
+}
