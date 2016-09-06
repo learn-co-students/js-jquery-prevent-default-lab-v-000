3c8daa3c4f60dfa5c9f94768ee9380c491bdf3dc
Saul <digitalh2o@gmail.com>
2016-09-06 00:23:02 -0500

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   // call functions here
+  preventDef();
 });
 
 // define functions here
+function submitForm() {
+	$('form').on('submit', function(event){
+		var item = $('#item').val();
+		//debugger
+		$('ol').append('<li>' + item + '</li>');
+		event.preventDefault();
+	});
+}
