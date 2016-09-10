9734012bf5c6c936621ee73dc8aed295c02bed2d
Adam Taitano <adam.taitano@gmail.com>
2016-09-10 20:44:55 +0000

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
   // call functions here
+  click();
 });
 
+var input;
 // define functions here
+
+
+function submitForm() {
+  $('input[type=submit]').on('submit', function(event) {
+    input = $('input').val()
+    $('ol').append("<li>" + input + "</li>");
+    event.preventDefault();
+  });
+};
