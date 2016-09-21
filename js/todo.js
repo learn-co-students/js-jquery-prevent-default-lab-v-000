1b552bdf970b758e4f7c787ac117cafc117d60ef
Megan Eloise <mev277@nyu.edu>
2016-09-21 14:50:42 +0200

    Done.

--- js/todo.js
+++ js/todo.js
@@ -5,9 +5,9 @@ $(document).ready(function(){
 // define functions here
 
 function submitForm() {
-  $('form').on('submit', function(event){
-    var listitem = $("#item").val();
-    $("ol".append("<li>" + listitem + "</li>");
+  $('form').on('submit', function (event){
+    var listItem = $('#item').val();
+    $("ol").append("<li>" + listItem + "</li>");
     event.preventDefault();
   })
 }
