af76049bb020e7ec387f0a4b752b65b0bc999837
kentleong <kentleong@github.com>
2016-09-22 18:03:49 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
+function submitForm() {
+  $('form').on('submit', function() {
+    var item = $('#item').val();
+    $('ol').append('<li>' + item + '</li>' );
+    event.preventDefault();
+  });
+}
 // define functions here
