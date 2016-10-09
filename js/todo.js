86a1264ffa6304bd83985564129c3840cb84054d
rgylling <mikegylling@yahoo.com>
2016-10-09 14:12:08 -0700

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-  // call functions here
+  submitForm()
 });
 
-// define functions here
+function submitForm() {
+  $('form').on('submit', function(e){
+    e.preventDefault();
+    var textField = $('#item').val();
+    $('ol').append('<li>' + textField + '</li>')
+  })
+}
