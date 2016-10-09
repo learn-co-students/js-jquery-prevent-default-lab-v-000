943121c5c8594acce6968ad52b384dae5d6a3ac6
Joseph Lim <joseph.d.lim@gmail.com>
2016-10-09 18:52:12 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 function submitForm() {
   $('form').on('submit', function(e){
     var item = $('#item').val();
-    $('#list ol').append(`<li>${item}</li>`);
+    $('#list ol').append("<li>"+item+"</li>");
     event.preventDefault();
   })
 }
