9de6eabb38aa592da340f98447e9d3466d9d4534
ThisIsTyDell <milleb94@gmail.com>
2016-10-27 15:01:46 -0500

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
+  submitForm();
   // call functions here
 });
 
 // define functions here
+
+function submitForm() {
+  $('form').on('submit', function(event) {
+    var listItem = $('#item').val();
+    $('ol').append('<li>' + listItem + '</li>');
+    event.preventDefault();
+  });
+}
