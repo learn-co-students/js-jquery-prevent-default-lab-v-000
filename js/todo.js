e0eb1cf93053dbe7a1d6e63db5f8e9d81d418850
lenn hypolite <lennhydesign@gmail.com>
2016-11-10 10:21:44 -0500

    Done

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+function submitForm(){
+  $('form').on('submit', function(event){
+    var textInput = $('#item').val();
+    $('ol').append('<li>'+ textInput+ '</li>');
+    event.preventDefault();
+  });
+}
