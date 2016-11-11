30d7734ed4b9dbd5c67e6683ef723a54126fcfb1
Ann Lee <aplee29@users.noreply.github.com>
2016-11-10 23:17:04 -0500

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+function submitForm() {
+  $('form').on('submit', function(event) {
+    var listItem = $('#item').val();
+    $('#list ol').append($('<li>').text(listItem));
+
+    // $('ol').append( '<li>' + listItem + '</li>' );
+
+    event.preventDefault();
+  });
+}
