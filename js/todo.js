81391aff9b021d7b91962fa0118b8999152a61b1
bedle88 <robey.mc@gmail.com>
2016-11-22 13:42:15 -0500

    done

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-  // call functions here
+  submitForm()
 });
 
-// define functions here
+function submitForm() {
+  $('form').on('submit', function(event) {
+    var newItem = $('#item').val();
+    $('ol').append('<li>' + newItem + '</li>');
+    event.preventDefault();
+  });
+}
