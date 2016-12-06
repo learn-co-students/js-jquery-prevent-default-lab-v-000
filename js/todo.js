2579713197c6de7c47a79d3df74e51faa957b39b
Villy Siu <villysiu@gmail.com>
2016-12-06 01:14:37 +0000

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+
+function submitForm(){
+  $('form').on('submit', function(event){
+    var i = $('#item').val();
+    alert (i)
+    $("ol").append("<li>" + i + "</il>");
+    event.preventDefault();
+        });
+
+};
