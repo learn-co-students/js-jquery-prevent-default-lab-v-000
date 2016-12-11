8144893e2170aecd98dda0e9aaf4bbe03f42dcbb
Yechiel K <chilikkal@gmail.com>
2016-12-11 12:16:30 -0500

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+function adds(item) {
+  $('#list ol').append('<li>' + item + '</li>')
+}
+
+function submitForm() {
+  $('form').on('submit', function(event){
+    var item = $('#item').val();
+    adds(item);
+    event.preventDefault();
+  });
+}
