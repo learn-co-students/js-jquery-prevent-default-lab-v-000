2e720bc925a5bd580365f0bd6aa284e75715a5db
ldgarber <leah.garber88@gmail.com>
2016-12-13 09:54:08 -0500

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
   // call functions here
+  submitForm(); 
 });
 
 // define functions here
+//
+function submitForm() {
+  $('form').on("submit", handleFormSubmit); 
+}
+
+function handleFormSubmit(e) {
+  var value = $('#item').val(); 
+  var listItem = "<li>" + value + "</li>"
+  $('#list ol').append(listItem); 
+  e.preventDefault(); 
+  return; 
+}
