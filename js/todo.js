b758a7a53ad6ef857914daf7b2d21fb136d50933
Brianna Titone <briannactitone@gmail.com>
2016-12-20 04:15:08 +0000

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-  // call functions here
-});
 
+console.log("foo");
+  submitForm();
+});
+function submitForm(){
+
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    var list = $('#list ol');
+    var value = $("#item").val();
+    list.append("<li>"+value+"</li>");
+
+  })
+}
 // define functions here
