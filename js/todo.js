3206283a104e7dff053c91d917e0cb8894588436
bmolina-nyc <bruce.molina.81@gmail.com>
2017-01-21 19:04:00 -0500

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
   // call functions here
+  addItem();
 });
 
 // define functions here
+
+function submit(){
+  $('input').on('submit', addItem)
+}
+
+
+function addItem(){
+    var listItem = $('input#item').val();
+    $('#list ol').append(listItem)
+    event.preventDefault();
+  }
