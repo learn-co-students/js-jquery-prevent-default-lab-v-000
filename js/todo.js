929bfbf06f79dfa664f1184d40213eb2855e75c1
Joe <jsbenning4@gmail.com>
2017-01-31 13:40:09 -0500

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
   // call functions here
+  $("form").on( "submit", submitForm);
 });
 
 // define functions here
+
+  function submitForm(event) {
+    var item = ($("#item").val());
+    $("ol").append(`<li>${item}</li>`);
+    event.preventDefault();
+  }
+
