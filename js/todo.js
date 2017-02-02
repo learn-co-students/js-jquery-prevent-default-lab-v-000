196a2a79651eb3ff489ed09d131fcb288144991f
lcorr8 <l.corr.v@gmail.com>
2017-02-02 13:45:27 +0100

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   // call functions here
+  addItem();
 });
 
 // define functions here
+
+function addItem(){
+  $('form').on('submit', function(event){
+    var item = $('#item').val();
+    $('ol').append("<li>" + item + "</li>");
+    event.preventDefault();
+  });
+};
